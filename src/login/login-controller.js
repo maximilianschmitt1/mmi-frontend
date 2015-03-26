d98e7dce4df1d398ea0d09782b3d15d7d5b35de8
Maximilian Schmitt <maximilian.schmitt@stud.uni-regensburg.de>
2015-03-26 11:50:27 +0100

    basic api integration

--- src/login/login-controller.js
+++ src/login/login-controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
-var LoginController = function($scope) {
+var LoginController = function($scope, $http, API_URL) {
   $scope.login = function(credentials) {
-    console.log(credentials);
+    $http
+      .post(API_URL + '/auth/authenticate', credentials)
+      .then(function(res) {
+        var response = res.data;
+
+        if (response.error) {
+          console.log(response.error.message);
+          return;
+        }
+
+        console.log(response);
+      });
   };
 };
 
--- src/signup/signup-controller.js
+++ src/signup/signup-controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
-var SignupController = function($scope) {
+var SignupController = function($scope, $http, API_URL) {
   $scope.signup = function(userData) {
-    console.log(userData);
+    $http
+      .post(API_URL + '/users', userData)
+      .then(function(res) {
+        var response = res.data;
+
+        if (response.error) {
+          console.log(response.error);
+          return;
+        }
+
+        console.log(response);
+      });
   };
 };
 
