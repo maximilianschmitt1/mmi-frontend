3f0d2d5b8649d7f29cdb7a22517cfbf4a15b93d1
Maximilian Schmitt <maximilian.schmitt@stud.uni-regensburg.de>
2015-04-29 14:21:48 +0200

    fix level indicator animation

--- src/level-indicator/level-indicator-directive.js
+++ src/level-indicator/level-indicator-directive.js
@@ -4,17 +4,22 @@ var levelIndicator = function($timeout) {
   return {
     template: '{{level}}',
     link: function(scope, el, attrs) {
-      var habit = scope.habit;
+      scope.$watch('habit', animateLevel);
 
-      if (habit.levelledUp) {
-        scope.level = habit._before.level.value;
-        $timeout(setLevel, 1000);
-      } else {
-        setLevel();
+      function animateLevel() {
+        var habit = scope.habit;
+
+        if (habit.levelledUp) {
+          scope.level = habit._before.level.value;
+          $timeout(setLevel, 1000);
+        } else {
+          setLevel();
+        }
       }
 
+
       function setLevel() {
-        scope.level = habit.level.value;
+        scope.level = scope.habit.level.value;
       }
     }
   };
